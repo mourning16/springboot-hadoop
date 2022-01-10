a8e0635ad06539df948ecb31fcecddd1ed749858
陶清晨 <776404387@qq.com>
2022-01-10 17:48:34 +0800

    #Learning For Hadoop

---
+++ springboot-hadoop-wordcount/src/main/java/cn/com/soft/hadoopwordcount/SpringbootHadoopWordcountApplication.java
@@ -0,0 +1,13 @@
+package cn.com.soft.hadoopwordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHadoopWordcountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHadoopWordcountApplication.class, args);
+    }
+
+}
---
+++ springboot-hadoop-wordcount/src/test/java/cn/com/soft/hadoopwordcount/SpringbootHadoopWordcountApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.soft.hadoopwordcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHadoopWordcountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
